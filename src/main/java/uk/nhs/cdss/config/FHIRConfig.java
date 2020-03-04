d304486e0489f0776cc87d89ddac776ed4d4ced3
Michael Houston <michael.houston3@nhs.net>
2020-03-04 15:13:22 +0000

    Generate valid numeric snomed codes and update FHIR config

--- src/main/java/uk/nhs/cdss/config/FHIRConfig.java
+++ src/main/java/uk/nhs/cdss/config/FHIRConfig.java
@@ -1,11 +1,32 @@
 package uk.nhs.cdss.config;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.api.annotation.ResourceDef;
 import ca.uhn.fhir.rest.client.api.IClientInterceptor;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.IHttpRequest;
 import ca.uhn.fhir.rest.client.api.IHttpResponse;
+import java.util.Arrays;
+import java.util.List;
 import org.hl7.fhir.dstu3.model.CareConnectCarePlan;
+import org.hl7.fhir.dstu3.model.CareConnectCareTeam;
+import org.hl7.fhir.dstu3.model.CareConnectCondition;
+import org.hl7.fhir.dstu3.model.CareConnectEncounter;
+import org.hl7.fhir.dstu3.model.CareConnectEpisodeOfCare;
+import org.hl7.fhir.dstu3.model.CareConnectHealthcareService;
+import org.hl7.fhir.dstu3.model.CareConnectLocation;
+import org.hl7.fhir.dstu3.model.CareConnectMedication;
+import org.hl7.fhir.dstu3.model.CareConnectMedicationRequest;
+import org.hl7.fhir.dstu3.model.CareConnectObservation;
+import org.hl7.fhir.dstu3.model.CareConnectOrganization;
+import org.hl7.fhir.dstu3.model.CareConnectPatient;
+import org.hl7.fhir.dstu3.model.CareConnectPractitioner;
+import org.hl7.fhir.dstu3.model.CareConnectProcedure;
+import org.hl7.fhir.dstu3.model.CareConnectProcedureRequest;
+import org.hl7.fhir.dstu3.model.CareConnectRelatedPerson;
+import org.hl7.fhir.dstu3.model.CareConnectSpecimen;
+import org.hl7.fhir.dstu3.model.CoordinateResource;
+import org.hl7.fhir.dstu3.model.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +44,33 @@ public class FHIRConfig {
   @Bean
   public FhirContext fhirContext() {
     FhirContext fhirContext = FhirContext.forDstu3();
-    fhirContext.setDefaultTypeForProfile("http://hl7.org/fhir/StructureDefinition/CarePlan",
-        CareConnectCarePlan.class);
+
+    List<Class<? extends Resource>> profiles = Arrays.asList(
+        CareConnectCarePlan.class,
+        CareConnectCareTeam.class,
+        CareConnectCondition.class,
+        CareConnectEncounter.class,
+        CareConnectEpisodeOfCare.class,
+        CareConnectHealthcareService.class,
+        CareConnectLocation.class,
+        CareConnectMedication.class,
+        CareConnectMedicationRequest.class,
+        CareConnectObservation.class,
+        CareConnectOrganization.class,
+        CareConnectPatient.class,
+        CareConnectPractitioner.class,
+        CareConnectProcedure.class,
+        CareConnectProcedureRequest.class,
+        CareConnectRelatedPerson.class,
+        CareConnectSpecimen.class);
+
+    for (Class<? extends Resource> profileClass : profiles) {
+      ResourceDef resourceDef = profileClass.getAnnotation(ResourceDef.class);
+      String profile = resourceDef.profile();
+      fhirContext.setDefaultTypeForProfile(profile, profileClass);
+    }
+
+    fhirContext.registerCustomType(CoordinateResource.class);
 
     return fhirContext;
   }
