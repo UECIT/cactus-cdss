6225c93a16903d52a5de8a0e026f627450f554dd
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-03-16 09:54:05 +0000

    NCTH-409: Store composition

--- src/main/java/uk/nhs/cdss/transform/out/QuestionnaireTransformer.java
+++ src/main/java/uk/nhs/cdss/transform/out/QuestionnaireTransformer.java
@@ -1,12 +1,17 @@
 package uk.nhs.cdss.transform.out;
 
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.hl7.fhir.dstu3.model.Enumerations.PublicationStatus;
 import org.hl7.fhir.dstu3.model.Enumerations.ResourceType;
+import org.hl7.fhir.dstu3.model.Narrative;
 import org.hl7.fhir.dstu3.model.Questionnaire;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.nhs.cdss.domain.Question;
+import uk.nhs.cdss.services.NarrativeService;
 import uk.nhs.cdss.transform.Transformer;
 import uk.nhs.cdss.transform.bundle.QuestionnaireBundle;
 
@@ -15,6 +20,7 @@ import uk.nhs.cdss.transform.bundle.QuestionnaireBundle;
 public class QuestionnaireTransformer implements Transformer<QuestionnaireBundle, Questionnaire> {
 
   private final QuestionTransformer questionTransformer;
+  private final NarrativeService narrativeService;
 
   @Value("${cds.fhir.server}")
   private String cdsServer;
@@ -27,8 +33,9 @@ public class QuestionnaireTransformer implements Transformer<QuestionnaireBundle
         .setExperimental(false)
         .addSubjectType(ResourceType.PATIENT.toCode());
 
-    bundle
-        .getQuestionnaire()
+    questionnaire.setText(buildNarrative(bundle.getQuestionnaire()));
+
+    bundle.getQuestionnaire()
         .getItems()
         .stream()
         .map(questionTransformer::transform)
@@ -37,11 +44,30 @@ public class QuestionnaireTransformer implements Transformer<QuestionnaireBundle
     return questionnaire;
   }
 
-  public String fullUrl(String id) {
+  private String fullUrl(String id) {
     return new StringJoiner("/")
         .add(cdsServer)
         .add("Questionnaire")
         .add(id)
         .toString();
+  }
+
+  private Narrative buildNarrative(uk.nhs.cdss.domain.Questionnaire questionnaire) {
+    var textLines = questionnaire.getItems()
+        .stream()
+        .map(Question::getText)
+        .map(text -> "'" + text + "'")
+        .collect(Collectors.toUnmodifiableList());
+
+    if (textLines.size() <= 1) {
+      return narrativeService.buildNarrative("Patient was asked: " + textLines.get(0));
+    }
+
+    var allLines = Stream.concat(
+          Stream.of("Patient was asked the following questions:"),
+          textLines.stream())
+        .collect(Collectors.toUnmodifiableList());
+
+    return narrativeService.buildNarrative(allLines);
   }
 }
