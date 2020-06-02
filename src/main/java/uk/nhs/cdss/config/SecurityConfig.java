425f8bc6d7863d2d3e4eb9f7763cf6bdf685e962
Michael Houston <michael.houston3@nhs.net>
2020-06-02 17:41:23 +0100

    Add comment to remove security exceptions

--- src/main/java/uk/nhs/cdss/config/SecurityConfig.java
+++ src/main/java/uk/nhs/cdss/config/SecurityConfig.java
@@ -25,6 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests()
+        // TODO CDSCT-71 - remove ServiceDefinition exception by proxying through EMS
+        // TODO CDSCT-218 - remove image exception by proxying through EMS
         .antMatchers(HttpMethod.GET, "/image/**", "/fhir/ServiceDefinition/**").permitAll()
         .anyRequest().authenticated()
         .and()
