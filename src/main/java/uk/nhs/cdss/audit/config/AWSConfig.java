182b18935ce5700ec84d1230d3ceae4bcd17c385
Charlie Marcus <charlie.marcus1@nhs.net>
2020-06-03 15:18:48 +0100

    can't run component tests without region so readding, thanks a lot aws

--- src/main/java/uk/nhs/cdss/audit/config/AWSConfig.java
+++ src/main/java/uk/nhs/cdss/audit/config/AWSConfig.java
@@ -1,5 +1,6 @@
 package uk.nhs.cdss.audit.config;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,9 @@ public class AWSConfig {
 
   @Bean
   public AmazonSQS sqsClient() {
-    return AmazonSQSClientBuilder.defaultClient();
+    return AmazonSQSClientBuilder.standard()
+        .withRegion(Regions.EU_WEST_2)
+        .build();
   }
 
 }
