a85d319e9df1c3c9a16505582c0fb8ad5ef0ec14
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-06-11 08:08:13 +0100

    Configured the AuditSession serialisation to serialise createdDate as an ISO format string.

--- src/main/java/uk/nhs/cdss/config/MapperConfig.java
+++ src/main/java/uk/nhs/cdss/config/MapperConfig.java
@@ -3,7 +3,9 @@ package uk.nhs.cdss.config;
 import com.arakelian.jackson.databind.EnumUppercaseDeserializerModifier;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,13 +16,21 @@ public class MapperConfig {
    * Provides an {@see ObjectMapper} that can deserialise enums in a case-insensitive way.
    * This is mainly useful for our {@see Concept} interfaces as we store them UPPERCASE_STYLE in the
    * Enum values (as per Java style) and in lowercase_style in the JSON resources.
+   * Additionally, this is configured to serialise Java 8 java.time objects (Instant, LocalDate &c.)
+   * as ISO 8601 dates, behaviour which was not yet the default in our version of jackson.
    * @return The mapper with the configured modifiers.
    */
   @Bean("enhanced")
   public ObjectMapper registryObjectMapper() {
+
+    var uppercaseEnumModule = new SimpleModule()
+        .setDeserializerModifier(new EnumUppercaseDeserializerModifier());
+
     return new ObjectMapper()
-        .configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false)
-        .registerModule(
-            new SimpleModule().setDeserializerModifier(new EnumUppercaseDeserializerModifier()));
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .disable(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES)
+        .registerModule(uppercaseEnumModule)
+        .registerModule(new JavaTimeModule());
   }
 }
