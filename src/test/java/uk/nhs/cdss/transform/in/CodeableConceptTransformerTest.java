8f56963de5170109fdcc70f84f1a989418a8a30b
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-06-30 15:25:18 +0100

    CodeableConceptTransformer tests.

---
+++ src/test/java/uk/nhs/cdss/transform/in/CodeableConceptTransformerTest.java
@@ -0,0 +1,52 @@
+package uk.nhs.cdss.transform.in;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CodeableConceptTransformerTest {
+
+  @InjectMocks
+  private CodeableConceptTransformer codeableConceptTransformer;
+
+  @Mock
+  private CodingInTransformer codingTransformer;
+
+  @Test(expected = NullPointerException.class)
+  public void transform_withNullConcept_shouldFail() {
+    //noinspection ConstantConditions
+    codeableConceptTransformer.transform(null);
+  }
+
+  @Test
+  public void transform_shouldTransform() {
+
+    var coding1 = new Coding();
+    var coding2 = new Coding();
+
+    var initialConcept = new CodeableConcept();
+    initialConcept.addCoding(coding1);
+    initialConcept.addCoding(coding2);
+    initialConcept.setText("validText");
+
+    var transformedCoding1 = new uk.nhs.cdss.domain.Coding("1", "1");
+    var transformedCoding2 = new uk.nhs.cdss.domain.Coding("2", "2");
+    when(codingTransformer.transform(coding1)).thenReturn(transformedCoding1);
+    when(codingTransformer.transform(coding2)).thenReturn(transformedCoding2);
+
+    var transformedConcept = codeableConceptTransformer.transform(initialConcept);
+
+    assertThat(transformedConcept.getCoding(), hasItems(transformedCoding1, transformedCoding2));
+    assertThat(transformedConcept.getText(), is("validText"));
+  }
+}
