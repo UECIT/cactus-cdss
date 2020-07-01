a8ecc9bd37d17b478d372ededab21c8a2e1f5646
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-07-01 11:22:51 +0100

    Tests for NarrativeService

---
+++ src/test/java/uk/nhs/cdss/services/NarrativeServiceTest.java
@@ -0,0 +1,47 @@
+package uk.nhs.cdss.services;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.springframework.util.StringUtils.countOccurrencesOf;
+
+import java.util.List;
+import org.hl7.fhir.dstu3.model.Narrative.NarrativeStatus;
+import org.junit.Test;
+
+public class NarrativeServiceTest {
+
+  private final NarrativeService narrativeService = new NarrativeService();
+
+  @Test
+  public void buildNarrative_withNull_shouldBuildNullNarrative() {
+    var returnedNarrative = narrativeService.buildNarrative((String) null);
+
+    assertThat(returnedNarrative.getDivAsString(), nullValue());
+  }
+
+  @Test
+  public void buildNarrative_withText_shouldBuildNarrative() {
+    var returnedNarrative = narrativeService.buildNarrative("uniqueText");
+
+    assertThat(returnedNarrative.getDivAsString(), containsString("uniqueText"));
+    assertThat(returnedNarrative.getStatus(), is(NarrativeStatus.GENERATED));
+  }
+
+  @Test
+  public void buildNarrative_withTextList_shouldBuildNarrative() {
+    var textList = List.of(
+        "we're only travellers",
+        "on this road",
+        "I've seen enough",
+        "to keep me going");
+
+    var returnedNarrative = narrativeService.buildNarrative(textList);
+    var returnedText = returnedNarrative.getDivAsString();
+
+    assertThat(countOccurrencesOf(returnedText, "<br"), is(3));
+    textList.forEach(text -> assertThat(returnedText, containsString(text)));
+    assertThat(returnedNarrative.getStatus(), is(NarrativeStatus.GENERATED));
+  }
+}
